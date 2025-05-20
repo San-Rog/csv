5649790bf39a1002d244cca9e58c6b3b9b4e52c6
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 07:55:02 -0300

    Update lessonCsv.py

--- lessonCsv.py
+++ lessonCsv.py
@@ -14,7 +14,7 @@ html = """
   </head>
   <body>    
     <p class="center">This paragraph refers to two classes.</p>
-    <p class="bott">{testeOne}</p>
+    <p class="bott">f"{testeOne}"</p>
     
     <p id="para1">Hello World - SOFREMOS TODAS AS DORES DO MUNDO!</p>
     <h1>My First CSS Example</h1>
