d7543a6139be8110bc4da17d096be792c3aaf110
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 07:45:10 -0300

    Update lessonCsv.py

--- lessonCsv.py
+++ lessonCsv.py
@@ -6,16 +6,15 @@ with open('teste.css') as f:
 html = """
 <hmtl>
   <head>
-
+    <h3>My First CSS Example - H3</h3>
+    <h4>My First CSS Example - H$</h4>
+    <h5>My First CSS Example - H5</h5>
   </head>
   <body>
     <p class="center">This paragraph refers to two classes.</p>
     <p id="para1">Hello World - SOFREMOS TODAS AS DORES DO MUNDO!</p>
     <h1>My First CSS Example</h1>
     <p>Sempre ganharemos a batalha.</p>
-    <h3>My First CSS Example - H3</h3>
-    <h4>My First CSS Example - H$</h4>
-    <h5>My First CSS Example - H5</h5>
   </body>
 </html>
 """
