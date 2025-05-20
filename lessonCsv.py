99987522b5822647a37a23c88d17e67904110bc5
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 08:04:56 -0300

    Update lessonCsv.py

--- lessonCsv.py
+++ lessonCsv.py
@@ -14,6 +14,7 @@ html = f"""
   </head>
   <body>    
     <p class="center">This paragraph refers to two classes.</p>
+    <p class="img">Imagem de fundo.</p>
     <p id="para1">Hello World - SOFREMOS TODAS AS DORES DO MUNDO!</p>
     <h1>My First CSS Example</h1>
     <p>Sempre ganharemos a batalha.</p>
