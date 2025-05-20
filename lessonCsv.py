f39fe7bb8245892866b4f7c830d2e2108d6398c5
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 09:32:33 -0300

    Update lessonCsv.py

--- lessonCsv.py
+++ lessonCsv.py
@@ -22,7 +22,7 @@ html = f"""
     <h5>Meu primeiro exemplo</h5>
   </head>
   <body>
-    <div>Ocupação de espaço na tela.</div>
+    <div><p>Ocupação de espaço na tela.</p></div>
     <p class="center">This paragraph refers to two classes.</p>
     <p class="img">Imagem de fundo sdfsdfsdfsdfsdfsdf.</p>
     <p id="para1">Hello World - SOFREMOS TODAS AS DORES DO MUNDO!</p>
