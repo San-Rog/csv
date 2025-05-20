4c2788102843b71fc6fd043c56ebee9edae31872
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 09:37:17 -0300

    Update lessonCsv.py

--- lessonCsv.py
+++ lessonCsv.py
@@ -22,7 +22,7 @@ html = f"""
     <h5>Meu primeiro exemplo</h5>
   </head>
   <body>
-    <p class="title">Ocupação de espaço com altura e largura predefinidos.</p>
+    <p class="ocup">Ocupação de espaço com altura e largura predefinidos.</p>
     <p class="center">This paragraph refers to two classes.</p>
     <p class="img">Imagem de fundo sdfsdfsdfsdfsdfsdf.</p>
     <p id="para1">Hello World - SOFREMOS TODAS AS DORES DO MUNDO!</p>
