8e0337303ce40b932c4a401a9d5b3b1a1971147e
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 10:17:39 -0300

    Update lessonCsv.py

--- lessonCsv.py
+++ lessonCsv.py
@@ -32,6 +32,6 @@ html = f"""
   </body>
 </html>
 """
-#st.markdown(html, unsafe_allow_html=True)
+st.markdown(html, unsafe_allow_html=True)
 #Maneiras de inserir CSS externo no streamlit.
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
