601e59f2291053e11e75a14adc66a684c4f71609
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-20 07:36:52 -0300

    Add files via upload

---
+++ lessonCsv.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+with open('teste.css') as f:
+    css = f.read()
+
+html = """
+<hmtl>
+  <body>
+    <p class="center">This paragraph refers to two classes.</p>
+    <p id="para1">Hello World - SOFREMOS TODAS AS DORES DO MUNDO!</p>
+    <h1>My First CSS Example</h1>
+    <p>Sempre ganharemos a batalha.</p>
+    <h3>My First CSS Example - H3</h3>
+    <h4>My First CSS Example - H$</h4>
+    <h5>My First CSS Example - H5</h5>
+  </body>
+</html>
+"""
+st.markdown(html, unsafe_allow_html=True)
+#Maneiras de inserir CSS externo no streamlit.
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+#st.markdown("<link rel='stylesheet'" "href={css}>", unsafe_allow_html=True)
